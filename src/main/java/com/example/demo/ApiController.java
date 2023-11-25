16f0715a5a42ac05734c17324a0d91cada1dd5c4
pa4oks <olgaromaskova39@gmail.com>
2023-11-25 23:30:39 +0300

    I've done

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -15,6 +15,7 @@ import java.util.List;
     @RestController
     public class ApiController {
         private final List<String> messages = new ArrayList<>();
+    private final List<User> users = new ArrayList<>();
         @GetMapping("messages")
         public ResponseEntity<List<String>> getMessages() {
             return ResponseEntity.ok(messages);
@@ -38,6 +39,66 @@ import java.util.List;
             messages.remove((int) i);
             messages.add(i, message);
             return ResponseEntity.accepted().build();
-        } // curl -X PUT localhost:8080/messages/0 -H "Content-Type: text/plain"
-    }
+        } // curl -X PUT localhost:8080/messages/0 -H "Content-Type: text/plain" -d "ABOBA"
+        @GetMapping("messages/search/{text}")
+        public ResponseEntity<Integer> getMessage(@PathVariable("text") String text) {
+            for (int i=0; i<messages.size(); i++){
+                if(messages.get(i).contains(text)){
+                    return ResponseEntity.ok(i);
+                }
+            }
+            return null;
+        } //curl -X GET localhost:8080/messages/search/ABOBA -H "Content-Type: text/plain"
+    @GetMapping("messages/search/count")
+    public ResponseEntity<Integer> countMessages(){
+                return ResponseEntity.ok(messages.size());
+    } //curl -X GET localhost:8080/messages/search/count -H "Content-Type: text/plain"
+    @PostMapping("messages/{index}/create")
+    public ResponseEntity<Void> addMessage(@RequestBody String text, @PathVariable("index") Integer index) {
+        messages.add(index, "NEW MESSAGE");
+        return ResponseEntity.accepted().build();
+    } // curl -X POST localhost:8080/messages/0/create -H "Content-Type: text/plain" -d "text"
+    @DeleteMapping("messages/search/{text}")
+    public ResponseEntity<Void> deleteText(@PathVariable("text") String text) {
+        int i=0;
+        while (i<messages.size()) {
+            if (messages.get(i).contains(text)) {
+                messages.remove(i);
+            }
+            else{
+                i++;
+            }
+        }
+        return ResponseEntity.noContent().build();
+    } //curl -X DELETE localhost:8080/messages/search/ABOBA -H "Content-Type: text/plain"
+
+
+    @PostMapping("users")
+    public ResponseEntity<Void> addUser(@RequestBody User user) {
+        users.add(user);
+        return ResponseEntity.accepted().build();
+    }// curl -X POST localhost:8080/users -H "Content-Type: application/json" -d "{\"name\":\"Bob\",\"age\":\"25\"}"
+
+    @DeleteMapping("users/{index}")
+    public ResponseEntity<Void> deleteUser(@PathVariable("index") Integer index) {
+        users.remove((int) index);
+        return ResponseEntity.noContent().build();
+    }// curl -X DELETE localhost:8080/users/0 -H "Content-Type: application/json"
+    @GetMapping("users/{index}")
+    public ResponseEntity<User> getUser(@PathVariable("index") Integer index) {
+        return ResponseEntity.ok(users.get(index));
+    }// curl -X GET localhost:8080/users/0 -H "Content-Type: application/json"
+    @GetMapping("users")
+    public ResponseEntity<List<User>> getUsers(){
+            return ResponseEntity.ok(users);
+    }// curl -X GET localhost:8080/users -H "Content-Type: application/json"
+    @PutMapping("users/{index}")
+    public ResponseEntity<Void> updateUserAge(@PathVariable("index") Integer i, @RequestBody Integer age) {
+        User user = users.get(i);
+        user.setAge(age);
+        users.remove((int) i);
+        users.add(i, user);
+        return ResponseEntity.accepted().build();
+    }// curl -X PUT localhost:8080/users/0 -H "Content-Type: application/json" -d "26"
+}
 
---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+public class User {
+    private String name;
+    private int age;
+
+    public User(int age, String name) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
